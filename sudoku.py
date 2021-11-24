3849bf882478c3497aae2b54982f440b26044935
Puckps <puck@swildens.net>
2021-11-24 15:31:42 +0100

    done

--- sudoku.py
+++ sudoku.py
@@ -37,71 +37,129 @@ class Sudoku:
 
     def value_at(self, x: int, y: int) -> int:
         """Returns the value at x,y."""
-        value = -1
 
-        for i in range(9):
-            for j in range(9):
-                if i == x and j == y:
-                    row = self._grid[y]
-                    value = int(row[x])
+        return int(self._grid[y][x])
 
-        return value
+        # for i in range(9):
+        #     for j in range(9):
+        #         if i == x and j == y:
+        #             return int(self._grid[y][x])
+        # return -1
+
+        # value = -1
+        # for i in range(9):
+        #     for j in range(9):
+        #         if i == x and j == y:
+        #             row = self._grid[y]
+        #             value = int(row[x])
+        # return value
 
     def options_at(self, x: int, y: int) -> Iterable[int]:
         """Returns all possible values (options) at x,y."""
+        
+        return self.options_at_row(y) & self.options_at_column(x) & self.options_at_block(x, y)
+
+        # options = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+        # # Remove all values from the row
+        # for value in self.row_values(y):
+        #     if value in options:
+        #         options.remove(value)
+
+        # # Remove all values from the column
+        # for value in self.column_values(x):
+        #     if value in options:
+        #         options.remove(value)
+
+        # block_index = (y // 3) * 3 + x // 3
+
+        # # Remove all values from the block
+        # for value in self.block_values(block_index):
+        #     if value in options:
+        #         options.remove(value)
+        
+        # return options
+
+    def options_at_row(self, y: int) -> Iterable[int]:
         options = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        return set(options) - set(self.row_values(y))
 
-        # Remove all values from the row
-        for value in self.row_values(y):
-            if value in options:
-                options.remove(value)
+        # options = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-        # Remove all values from the column
-        for value in self.column_values(x):
-            if value in options:
-                options.remove(value)
+        # # Remove all values from the row
+        # for value in self.row_values(y):
+        #     if value in options:
+        #         options.remove(value)
+        # return options
 
-        # Get the index of the block based from x,y
+    def options_at_column(self, x: int) -> Iterable[int]:
+        options = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        return set(options) - set(self.column_values(x))
+        
+        # options = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        
+        # # Remove all values from the column
+        # for value in self.column_values(x):
+        #     if value in options:
+        #         options.remove(value)
+        # return options
+
+    def options_at_block(self, x: int, y: int) -> Iterable[int]:
+        options = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         block_index = (y // 3) * 3 + x // 3
+        return set(options) - set(self.block_values(block_index))
+       
+        # options = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        # block_index = (y // 3) * 3 + x // 3
 
-        # Remove all values from the block
-        for value in self.block_values(block_index):
-            if value in options:
-                options.remove(value)
-
-        return options
+        # # Remove all values from the block
+        # for value in self.block_values(block_index):
+        #     if value in options:
+        #         options.remove(value)
+        # return options
 
     def next_empty_index(self) -> tuple[int, int]:
         """
         Returns the next index (x,y) that is empty (value 0).
         If there is no empty spot, returns (-1,-1)
         """
-        next_x, next_y = -1, -1
 
         for y in range(9):
             for x in range(9):
-                if self.value_at(x, y) == 0 and next_x == -1 and next_y == -1:
-                    next_x, next_y = x, y
+                if self.value_at(x, y) == 0:
+                    return x, y
+        return -1, -1
 
-        return next_x, next_y
+        # next_x, next_y = -1, -1
+        # for y in range(9):
+        #     for x in range(9):
+        #         if self.value_at(x, y) == 0 and next_x == -1 and next_y == -1:
+        #             next_x, next_y = x, y
+        # return next_x, next_y
 
     def row_values(self, i: int) -> Iterable[int]:
         """Returns all values at i-th row."""
-        values = []
 
-        for j in range(9):
-            values.append(self.value_at(j, i))
+        return [self.value_at(j, i) for j in range(9)]
 
-        return values
+        # values = []
+
+        # for j in range(9):
+        #     values.append(self.value_at(j, i))
+
+        # return values
 
     def column_values(self, i: int) -> Iterable[int]:
         """Returns all values at i-th column."""
-        values = []
 
-        for j in range(9):
-            values.append(self.value_at(i, j))
+        return [self.value_at(i, j) for j in range(9)]
 
-        return values
+        # values = []
+
+        # for j in range(9):
+        #     values.append(self.value_at(i, j))
+
+        # return values
 
     def block_values(self, i: int) -> Iterable[int]:
         """
@@ -127,22 +185,26 @@ class Sudoku:
         Returns True if and only if all rows, columns and blocks contain
         only the numbers 1 through 9. False otherwise.
         """
-        values = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-        result = True
-
         for i in range(9):
-            for value in values:
-                if value not in self.column_values(i):
-                    result = False
+            if 0 in self.column_values(i) or 0 in self.row_values(i) or 0 in self.block_values(i):
+                return False
+        return True
+        
+        # values = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        # result = True
+        
+        # for i in range(9):
+        #     for value in values:
+        #         if value not in self.column_values(i):
+        #             result = False
 
-                if value not in self.row_values(i):
-                    result = False
+        #         if value not in self.row_values(i):
+        #             result = False
 
-                if value not in self.block_values(i):
-                    result = False
+        #         if value not in self.block_values(i):
+        #             result = False
 
-        return result
+        # return result
 
     def __str__(self) -> str:
         representation = ""
